5acf81da744a19577e26468c91cd12fd702edda2
ItsKevinLorenzo <132334722+ItsKevinLorenzo@users.noreply.github.com>
2025-03-18 17:09:09 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre válido.");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    actualizarLista();
+    inputAmigo.value = "";
+    inputAmigo.focus();
+}
+
+function actualizarLista() {
+    const listaAmigoUI = document.getElementById("listaAmigos");
+    listaAmigoUI.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigoUI.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear. Agrega nombres primero.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultadoUI = document.getElementById("resultado");
+    resultadoUI.innerHTML = `<li>${amigoSorteado}</li>`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".button-add").addEventListener("click", agregarAmigo);
+    document.querySelector(".button-draw").addEventListener("click", sortearAmigo);
+});
